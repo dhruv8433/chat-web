223deea6418d0a7bbc16404e79e141c8134ee7d6
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-09 12:43:01 +0530

    reusable options

---
+++ app/common/Options.jsx
@@ -0,0 +1,28 @@
+import { Divider, Icon } from "@mui/material";
+import React from "react";
+
+const Options = ({ OptionIcon, title, callPage }) => {
+  return (
+    <>
+      <div className="flex items-center rounded-2xl h-20 relative hover:cursor-pointer hover:bg-[#494c55]">
+        {/* icons */}
+        <div className="ml-3">
+          <Icon>{<OptionIcon />}</Icon>
+        </div>
+        {/* content */}
+        <div className="">
+          <h1 className="text-xl ml-4">{title}</h1>
+          {/* if it's calls page than only visible time and call type */}
+          {callPage && (
+            <p>
+              12:20 <span>voice call</span>
+            </p>
+          )}
+        </div>
+      </div>
+      <Divider />
+    </>
+  );
+};
+
+export default Options;
