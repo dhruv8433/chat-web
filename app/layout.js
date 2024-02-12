9b587dd777be9de97c4a7cccd3a9dfbc1cfb7007
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-12 20:16:58 +0530

    global layout

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,50 @@
+"use client";
+
 import "./globals.css";
 import "./style/style.css";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar";
 import { Baloo_2 } from "next/font/google";
+import { darkTheme, lightTheme } from "./themes/theme";
+import { useEffect, useState } from "react";
 
 const font = Baloo_2({
   subsets: ["vietnamese"],
   weight: ["400", "500", "600", "700", "800"],
 });
 
-export const metadata = {
-  title: "Chats | Chattings",
-};
-
 export default function RootLayout({ children }) {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  function changeDarkMode() {
+    setTheme("dark");
+  }
+
+  function changeLightMode() {
+    setTheme("light");
+  }
   return (
     <html lang="en">
-      <body className={`${font.className} m-10`}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={2}>
-            <Box
-              className="fixed w-[15%]"
-              display={{ xs: "none", md: "block" }}
-            >
-              <Navbar />
-            </Box>
+      <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+        <body className={`${font.className} m-10`}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={2}>
+              <Box
+                className="fixed w-[15%]"
+                display={{ xs: "none", md: "block" }}
+              >
+                <Navbar
+                  changeDarkMode={changeDarkMode}
+                  changeLightMode={changeLightMode}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={10}>
+              {children}
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={10}>
-            {children}
-          </Grid>
-        </Grid>
-      </body>
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
