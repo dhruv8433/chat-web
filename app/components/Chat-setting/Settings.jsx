6b593f59cfd06e57f105bc6a85094f49e0ed4d18
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-13 19:47:28 +0530

    settings page

--- app/components/Chat-setting/Settings.jsx
+++ app/components/Chat-setting/Settings.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Options from "@/app/common/Options";
-import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import UpdateIcon from '@mui/icons-material/Update';
-import HistoryIcon from '@mui/icons-material/History';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import SupervisorAccountOutlinedIcon from '@mui/icons-material/SupervisorAccountOutlined';
-import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
-import StarBorderPurple500OutlinedIcon from '@mui/icons-material/StarBorderPurple500Outlined';
+import ColorLensOutlinedIcon from "@mui/icons-material/ColorLensOutlined";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import UpdateIcon from "@mui/icons-material/Update";
+import HistoryIcon from "@mui/icons-material/History";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import SupervisorAccountOutlinedIcon from "@mui/icons-material/SupervisorAccountOutlined";
+import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
+import StarBorderPurple500OutlinedIcon from "@mui/icons-material/StarBorderPurple500Outlined";
 import Link from "next/link";
 
 const Settings = () => {
@@ -14,19 +15,25 @@ const Settings = () => {
     <div className="min-h-[770px] primary border border-black rounded-2xl">
       <div className="p-2 ">
         {/* Profile */}
-        <Options OptionIcon={<PersonOutlineIcon className="h-full" sx={{height: 20, width: 20}} />} title={"Profile"} />
+        <Options OptionIcon={PersonOutlineIcon} title={"Profile"} />
         {/* your activity */}
-        <Options OptionIcon={<UpdateIcon />} title={"Your Activity"} />
+        <Options OptionIcon={UpdateIcon} title={"Your Activity"} />
         {/* Archived */}
-        <Options OptionIcon={<HistoryIcon />} title={"Archived"} />
+        <Options OptionIcon={HistoryIcon} title={"Archived"} />
         {/* Saved */}
-        <Options OptionIcon={<BookmarkBorderIcon />} title={"Saved"} />
+        <Options OptionIcon={BookmarkBorderIcon} title={"Saved"} />
         {/* Supervision */}
-        <Options OptionIcon={<SupervisorAccountOutlinedIcon />} title={"Supervision"} />
+        <Options
+          OptionIcon={SupervisorAccountOutlinedIcon}
+          title={"Supervision"}
+        />
         {/* Verified */}
-        <Options OptionIcon={<CheckCircleOutlinedIcon />} title={"Verified"} />
+        <Options OptionIcon={CheckCircleOutlinedIcon} title={"Verified"} />
         {/* Close Friends */}
-        <Options OptionIcon={<StarBorderPurple500OutlinedIcon />} title={"Close Friends"} />
+        <Options
+          OptionIcon={StarBorderPurple500OutlinedIcon}
+          title={"Close Friends"}
+        />
       </div>
     </div>
   );
