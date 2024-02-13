b7127db13c101a639d6c171288f4676c09f50644
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-13 18:30:19 +0530

    global layout

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,12 @@
 
 import "./globals.css";
 import "./style/style.css";
-import { Box, Grid, ThemeProvider } from "@mui/material";
+import { Box, Grid, ThemeProvider, useTheme } from "@mui/material";
 import Navbar from "./components/Navbar";
 import { Baloo_2 } from "next/font/google";
 import { darkTheme, lightTheme } from "./themes/theme";
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import Heading from "./common/Heading";
 
 const font = Baloo_2({
   subsets: ["vietnamese"],
@@ -14,37 +15,44 @@ const font = Baloo_2({
 });
 
 export default function RootLayout({ children }) {
-  const [theme, setTheme] = useState(localStorage.getItem("theme"));
+  const [isDarkTheme, setDarkTheme] = useState(localStorage.getItem("theme"));
 
-  function changeDarkMode() {
-    setTheme("dark");
-    localStorage.setItem("theme", "dark");
+  function DarkThemeApplied() {
+    setDarkTheme(true);
+    localStorage.setItem("theme", true);
   }
 
-  function changeLightMode() {
-    setTheme("light");
-    localStorage.setItem("theme", "light");
+  function lightThemeApplied() {
+    setDarkTheme(false);
+    localStorage.setItem("theme", false);
   }
+
+  const theme = useTheme();
+  console.log("palletes: ", theme);
   return (
     <html lang="en">
-      <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
-        <body className={`${font.className} m-10`}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={2}>
-              <Box
-                className="fixed w-[15%]"
-                display={{ xs: "none", md: "block" }}
-              >
-                <Navbar
-                  changeDarkMode={changeDarkMode}
-                  changeLightMode={changeLightMode}
+      <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+        <body className={`${font.className}`}>
+          <Box bgcolor={theme.palette.background.paper} className="m-10">
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={2}>
+                <Box
+                  className="fixed w-[15%]"
+                  display={{ xs: "none", md: "block" }}
+                >
+                  <Navbar />
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={10}>
+                <Heading
+                  title={"Chats"}
+                  DarkThemeApplied={DarkThemeApplied}
+                  lightThemeApplied={lightThemeApplied}
                 />
-              </Box>
+                <div>{children}</div>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={10}>
-              {children}
-            </Grid>
-          </Grid>
+          </Box>
         </body>
       </ThemeProvider>
     </html>
