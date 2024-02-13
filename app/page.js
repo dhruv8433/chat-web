ce79d0df40e0cd9de2510cab06b521ea6d81a225
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-13 18:30:23 +0530

    home page

--- app/page.js
+++ app/page.js
@@ -2,32 +2,20 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Heading from "./common/Heading";
 import { SingleChat } from "@/app/components/Chats/ChattingComponent";
 import { AllChats } from "@/app/components/Inbox/InboxComponent";
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#23262f" : "#23262f",
-  padding: theme.spacing(3),
-  textAlign: "center",
-  color: "white",
-}));
 
 export default function Home() {
   return (
     <Box>
-      <Heading title={"Chats"} />
-      <Grid container spacing={2} sx={{mt: 1}}>
+      {/* <Heading title={"Chats"} /> */}
+      <Grid container spacing={2} sx={{ mt: 1 }}>
         <Grid item xs={4}>
-          <Item className="rounded-2xl">
-            <AllChats />
-          </Item>
+          <AllChats />
         </Grid>
         <Grid item xs={8}>
-          <Item className="rounded-2xl">
-            <SingleChat />
-          </Item>
+          <SingleChat />
         </Grid>
       </Grid>
     </Box>
