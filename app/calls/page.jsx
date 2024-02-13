8470b3fc7b37ddf557ae1de688a95de7efa6688c
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-13 18:30:30 +0530

    pages

--- app/calls/page.jsx
+++ app/calls/page.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Heading from "../common/Heading";
 import Call from "../components/Calls/Call";
 
 const page = () => {
   return (
     <div>
-      <Heading title={"Calls"} />
+      {/* <Heading title={"Calls"} /> */}
       <Call />
     </div>
   );
--- app/chats/page.jsx
+++ app/chats/page.jsx
@@ -2,16 +2,14 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Heading from "../common/Heading";
 import { AllChats } from "../components/Inbox/InboxComponent";
 import { SingleChat } from "../components/Chats/ChattingComponent";
 
 export default function Home() {
   return (
     <Box>
-      <Heading title={"Chats"} />
+      {/* <Heading title={"Chats"} /> */}
       <Grid container spacing={2} sx={{ mt: 1 }}>
         <Grid item xs={12} md={4}>
           <AllChats />
