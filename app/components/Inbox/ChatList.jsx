701b749b560cf720cb4bddafec28451b7113f400
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-13 18:31:07 +0530

    chat list

--- app/components/Inbox/ChatList.jsx
+++ app/components/Inbox/ChatList.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { red } from "@mui/material/colors";
-import { Avatar } from "@mui/material";
+import { Avatar, Box, useTheme } from "@mui/material";
 
 export default function ChatList() {
+  const theme = useTheme();
   return (
-    <div className="primary">
-      <div className="flex text px-4 hover:secondary hover:cursor-pointer py-2 mb-2 rounded-2xl">
-        <div className="">
+    <Box bgcolor={theme.palette.primary.main} className="rounded-2xl">
+      <div className="flex text px-4 hover:cursor-pointer py-2 mb-2 rounded-2xl">
+        <div>
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
             R
           </Avatar>
@@ -20,7 +21,7 @@ export default function ChatList() {
         </div>
       </div>
 
-      <div className="flex text px-4 primary hover:secondary hover:cursor-pointer py-2 mb-2 rounded-2xl">
+      <div className="flex text px-4 hover:cursor-pointer py-2 mb-2 rounded-2xl">
         <div className="">
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
             R
@@ -34,6 +35,6 @@ export default function ChatList() {
           <h1>September 14, 2016</h1>
         </div>
       </div>
-    </div>
+    </Box>
   );
 }
