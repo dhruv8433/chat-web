4eb5ecc389b96a0c1ad3b498e4b04f9b60cda47a
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-14 20:04:48 +0530

    home page

--- app/page.js
+++ app/page.js
@@ -1,26 +1,22 @@
 "use client";
+
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { SingleChat } from "@/app/components/Chats/ChattingComponent";
 import { AllChats } from "@/app/components/Inbox/InboxComponent";
-import LandingPage from "./components/LandingPage";
-
 
 export default function Home() {
   return (
-    <Box>
-
-      <Grid container spacing={2} sx={{ mt: 1 }}>
+    <div>
+      {/* <Grid container spacing={2} sx={{ mt: 1 }}>
         <Grid item xs={4}>
           <AllChats />
         </Grid>
         <Grid item xs={8}>
           <SingleChat />
         </Grid>
-      </Grid>
-
-    </Box>
+      </Grid> */}
+      <h1 className="text-white">Pending...</h1>
+    </div>
   );
 }
