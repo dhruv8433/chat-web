2051e9c195f2134652ce0a3eb5673611c0c345df
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-15 16:08:20 +0530

    landing page

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -1,73 +1,153 @@
 "use client";
 
+import { Grid, Paper } from "@mui/material";
 import React, { useState } from "react";
-import { Grid, Typography } from "@mui/material";
-import LoginModel from "../model/LoginModel";
+import MyBox from "../common/MyBox";
+import MyText from "../common/MyText";
+import MyButton from "../common/MyButton";
+import ChattingMiniAnimation from "@/app/animations/chatting.json";
+import VideoCallAnimation from "@/app/animations/video-call.json";
+import ExploreAnimation from "@/app/animations/explore.json";
+import ThemeAnimation from "@/app/animations/theme.json";
+import PostActionAnimation from "@/app/animations/postaction.json";
+import Feature from "../common/Feature";
+import { motion } from "framer-motion";
+import MyModel from "../common/MyModel";
+import SignUpModel from "../model/SignUpModel";
 
 const LandingPage = () => {
-  const [openModal, setOpenModal] = useState(false);
-
-  const handleOpenModal = () => {
-    setOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
+  const [signupModalOpen, setsignupModalOpen] = useState(false);
 
   return (
-    <Grid
-      container
-      spacing={4}
-      justifyContent="center"
-      alignItems="center"
-      style={{
-        backgroundColor: "white",
-        border: "80px solid orange",
-        minHeight: "100vh",
-        padding: "20px",
-      }}
-    >
-      {/* Left Side */}
-      <Grid item xs={12} md={6}>
-        <div style={{ textAlign: "center" }}>
-          <div style={{ marginBottom: "10px" }}>
-            <Typography variant="h1">Let's connect</Typography>
-          </div>
-          <div style={{ marginBottom: "10px" }}>
-            <Typography variant="h1">with your customer</Typography>
-          </div>
-          <div style={{ marginBottom: "10px" }}>
-            <Typography variant="h1">in real time</Typography>
-          </div>
-          <Typography variant="h6">
-            Your customers are waiting to chat with you. Start chatting now to
-            provide real-time support and build better relationships.
-          </Typography>
-          <button
-            className="button h-min w-max p-5 rounded m-auto mr-2 text-2xl capitalize"
-            onClick={handleOpenModal}
-          >
-            Start Chatting Now
-          </button>
-          {/* Render LoginModel only if openModal is true */}
-          {openModal && (
-            <LoginModel open={openModal} onClose={handleCloseModal} />
-          )}
-        </div>
-      </Grid>
+    <div>
+      <Paper>
+        <MyBox className={"h-full"}>
+          {/* Grid content */}
+          <Grid container>
+            <Grid item xs={12} md={4}>
+              <motion.div className="flex flex-col p-8 justify-center h-full">
+                <MyBox>
+                  {/* animation 1 sec */}
+                  <motion.div
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                  >
+                    <MyText
+                      className={
+                        "md:text-6xl sm:text-4xl mb-4 text-[#898cce] font-bold"
+                      }
+                    >
+                      New Generation Chat Web
+                    </MyText>
 
-      {/* Right Side */}
-      <Grid item xs={12} md={6}>
-        <div style={{ textAlign: "center" }}>
-          <img
-            src="/landImg.png"
-            alt="Chatting"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-      </Grid>
-    </Grid>
+                    <MyText className={"text-xl"}>
+                      "Chatting is more than just exchanging words; it's about
+                      connecting hearts, bridging distances, and creating bonds
+                      that transcend time and space."
+                    </MyText>
+
+                    <MyText className={"text-xl"}>
+                      Connect with friends, family, and colleagues in real-time
+                      with our secure and reliable chat platform.
+                    </MyText>
+                  </motion.div>
+                  <MyBox className={""}>
+                    {/* on button click myFunction Executed -- onClick Event */}
+                    <MyButton
+                      myFunction={() => setsignupModalOpen(true)}
+                      className={"text-start mt-4 px-2 py-1 rounded"}
+                    >
+                      Sign Up Now
+                    </MyButton>
+                  </MyBox>
+                </MyBox>
+              </motion.div>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <img
+                src="https://cdn.dribbble.com/users/291221/screenshots/5601520/media/94c902c5cf585645af1a3cfb16335acf.gif"
+                height={"500px"}
+                width={"100%"}
+                alt=""
+              />
+            </Grid>
+          </Grid>
+
+          {/* Signup Model */}
+          <MyModel
+            open={signupModalOpen}
+            setOpen={setsignupModalOpen}
+            className={"flex justify-center items-center "}
+          >
+            <SignUpModel />
+          </MyModel>
+
+          {/* Feature */}
+          <MyBox className={"w-full text-center py-5"}>
+            <MyText className={"text-4xl font-semibold"}>
+              Why to Consider This Chatting Web?
+            </MyText>
+            <MyBox className={"flex mt-4 justify-around"}>
+              <Grid container className="px-60" spacing={3}>
+                {/* Feature - 1 */}
+                <Feature
+                  animationData={ChattingMiniAnimation}
+                  md={4}
+                  heading={"Real time Chatting"}
+                  subHeading={
+                    "Real-time chatting sparks instant connections, fostering conversations that transcend boundaries. With each keystroke, words bridge gaps, emotions resonate, and friendships flourish. From fleeting hellos to heartfelt exchanges, it's a digital symphony of voices, uniting hearts and minds across the virtual landscape"
+                  }
+                />
+
+                {/* Feature - 2 */}
+                <Feature
+                  animationData={VideoCallAnimation}
+                  md={4}
+                  heading={"Video Calling"}
+                  subHeading={
+                    "Video calling brings distant faces close, weaving a tapestry of shared moments in real-time. Through pixels and screens, voices echo, laughter fills the air, and expressions paint stories. It's more than just a call; it's a window to worlds, connecting souls across miles with warmth and intimacy. you can also share your emotions with your family."
+                  }
+                />
+
+                {/* Feature - 3 */}
+                <Feature
+                  animationData={ExploreAnimation}
+                  md={4}
+                  heading={"Explore Eveything"}
+                  subHeading={
+                    "Exploring trending social media is like embarking on a digital adventure, where every click unveils a new narrative. It's a dynamic landscape of ideas, opinions, and creativity, where connections flourish and trends spark conversations. Navigating through feeds, we discover the pulse of society, shaping and reshaping our digital footprint."
+                  }
+                />
+
+                {/* Feature - 4 */}
+                <Feature
+                  animationData={ThemeAnimation}
+                  md={4}
+                  heading={"Dark Light Modes"}
+                  subHeading={
+                    "Dark mode themes immerse users in a sleek, sophisticated ambiance, reducing eye strain and enhancing focus during nocturnal browsing. Conversely, light mode themes exude vibrancy and clarity, ideal for daytime activities, offering a refreshing visual experience that resonates with the ambiance of a bright, sunlit environment."
+                  }
+                />
+
+                {/* Feature - 5 */}
+                <Feature
+                  animationData={PostActionAnimation}
+                  md={8}
+                  heading={"Like, Comment & Share"}
+                  subHeading={
+                    "Interactions such as likes, comments, and shares on a particular post play a pivotal role in shaping digital discourse and community engagement. They signify user appreciation, spark meaningful conversations, and amplify content reach across social networks"
+                  }
+                  subHeading1={
+                    "Each action serves as a catalyst for connection, fostering a sense of belonging and facilitating the exchange of ideas, opinions, and emotions within the online ecosystem."
+                  }
+                />
+              </Grid>
+            </MyBox>
+          </MyBox>
+        </MyBox>
+      </Paper>
+    </div>
   );
 };
 
