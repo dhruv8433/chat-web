8df2ddc3dafde73259492049f18b96f2b8c0b0a3
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-19 19:59:16 +0530

    common component modifications

--- app/common/MyButton.jsx
+++ app/common/MyButton.jsx
@@ -1,7 +1,14 @@
 import { useTheme } from "@mui/material";
 import React from "react";
 
-const MyButton = ({ children, className, myFunction, type }) => {
+const MyButton = ({
+  children,
+  className,
+  myFunction,
+  type,
+  isPrimaryButton,
+  customBgColor,
+}) => {
   const theme = useTheme();
   return (
     <button
@@ -9,7 +16,10 @@ const MyButton = ({ children, className, myFunction, type }) => {
       onClick={() => (myFunction ? myFunction() : "")}
       style={{
         color: theme.palette.background.text,
-        backgroundColor: theme.palette.background.button,
+        // if button is primary than blue bg else user specified color
+        backgroundColor: isPrimaryButton
+          ? theme.palette.background.button
+          : customBgColor,
       }}
       className={`${className}`}
       type={type}
