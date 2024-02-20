7c4fc065386007511715339748a048b027aef17e
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-20 08:09:53 +0530

    logout model

--- app/model/LogoutModel.jsx
+++ app/model/LogoutModel.jsx
@@ -1,51 +1,35 @@
-import { Button, Divider, Typography, Modal, Box } from "@mui/material";
+import { Button, Divider, Modal, Box } from "@mui/material";
 import React from "react";
+import MyBox from "../common/MyBox";
+import MyText from "../common/MyText";
+import MyButton from "../common/MyButton";
 
 const LogoutModel = ({ setLogoutDialogOpen }) => {
   return (
-    <Modal
-      open={true} // You can manage the modal's open state from parent component
-      onClose={() => setLogoutDialogOpen(false)} // Handle close action
-      aria-labelledby="logout-modal"
-      aria-describedby="logout-modal-description"
-    >
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          backgroundColor: "white",
-          border: "2px solid #000",
-          boxShadow: 24,
-          p: 4,
-          minWidth: 500,
-          maxWidth: 1000,
-          borderRadius: 4,
-        }}
-      >
-        <Typography variant="h1" sx={{ fontSize: "2rem" }} gutterBottom>
-          Logout
-        </Typography>
-        <Divider />
-        <Typography variant="body1" sx={{ mt: 2, textWrap: "wrap" }}>
-          Are you sure , Do you want to logout ?
-        </Typography>
-        {/* action buttons */}
-        <div
-          style={{ display: "flex", justifyContent: "flex-end", marginTop: 20 }}
+    <MyBox className={"p-4 rounded-2xl"}>
+      <MyText className={"text-2xl"}>Logout</MyText>
+      <Divider />
+      <MyText className={"mt-4"}>Are you sure , Do you want to logout ?</MyText>
+
+      {/* action buttons */}
+      <div className="flex justify-end m-3 gap-2">
+        <MyButton
+          myFunction={() => setLogoutDialogOpen(false)}
+          isPrimaryButton={false}
+          customBgColor={"gray"}
+          className={"px-1 py-2 rounded-lg"}
         >
-          <Button
-            variant="outlined"
-            onClick={() => setLogoutDialogOpen(false)}
-            sx={{ mr: 1 }}
-          >
-            Cancel
-          </Button>
-          <Button className="bg-red-700 text-white p-4">Logout</Button>
-        </div>
-      </Box>
-    </Modal>
+          Cancel
+        </MyButton>
+        <MyButton
+          isPrimaryButton={false}
+          customBgColor={"red"}
+          className={"p-1 rounded-lg"}
+        >
+          Logout
+        </MyButton>
+      </div>
+    </MyBox>
   );
 };
 
