fe20849f4cc6d8b1beddaba6df1b05e048d04c4a
v_a_r_s_a_n_i <137641079+Varsani2520@users.noreply.github.com>
2024-02-21 10:54:00 +0530

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,14 +1,35 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAYpXrXX6H4Y8MclOJaVjxh-vXehdFZZHE",
+//   authDomain: "chat-app-b0bf8.firebaseapp.com",
+//   projectId: "chat-app-b0bf8",
+//   storageBucket: "chat-app-b0bf8.appspot.com",
+//   messagingSenderId: "111914612720",
+//   appId: "1:111914612720:web:2aaf9fac39bf7f81a88f35",
+//   measurementId: "G-P6YZY29LT1",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export { app };
+// export default app;
+
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYpXrXX6H4Y8MclOJaVjxh-vXehdFZZHE",
-  authDomain: "chat-app-b0bf8.firebaseapp.com",
-  projectId: "chat-app-b0bf8",
-  storageBucket: "chat-app-b0bf8.appspot.com",
-  messagingSenderId: "111914612720",
-  appId: "1:111914612720:web:2aaf9fac39bf7f81a88f35",
-  measurementId: "G-P6YZY29LT1",
+  apiKey: "AIzaSyAiNA_1lcHeCExwEoAKeRemKA3y1cxGNi8",
+  authDomain: "chat-web-app-1c93d.firebaseapp.com",
+  projectId: "chat-web-app-1c93d",
+  storageBucket: "chat-web-app-1c93d.appspot.com",
+  messagingSenderId: "143006852403",
+  appId: "1:143006852403:web:4b29efee60ff340f0b4a46",
+  measurementId: "G-6NNLKSKE4T"
 };
 
 // Initialize Firebase
@@ -16,3 +37,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export { app };
 export default app;
+const analytics = getAnalytics(app);
