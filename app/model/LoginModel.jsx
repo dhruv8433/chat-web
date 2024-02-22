3c4ac93cc9c1039f6862a5bc63d4ab4a66be2c24
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-22 17:43:33 +0530

    login functionality

--- app/model/LoginModel.jsx
+++ app/model/LoginModel.jsx
@@ -5,22 +5,71 @@ import MyText from "../common/MyText";
 import MyButton from "../common/MyButton";
 import MyModel from "../common/MyModel";
 import SignUpModel from "./SignUpModel";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { loginUserSuccess, logoutuser } from "../action/action";
+import Cookies from "js-cookie";
+import { auth } from "../firebase";
+import { useDispatch } from "react-redux";
 
 const LoginModel = ({ setLoginModel }) => {
   const [signupModel, setSignupModel] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    // Handle login logic
-    console.log("Logging in with email:", email, "and password:", password);
+  const dispatch = useDispatch();
+
+  // const handleLogin = () => {
+  //   // Handle login logic
+  //   console.log("Logging in with email:", email, "and password:", password);
+  // };
+
+  // const handleLoginWithGoogle = () => {
+  //   // Handle login with Google logic
+  //   console.log("Logging in with Google");
+  // };
+
+  const signIn = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      dispatch(loginUserSuccess(user));
+      Cookies.set("user-auth", true);
+    } catch (err) {
+      console.error(err);
+      Cookies.set("user-auth", false);
+    }
   };
 
-  const handleLoginWithGoogle = () => {
-    // Handle login with Google logic
-    console.log("Logging in with Google");
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, new GoogleAuthProvider());
+      const user = result.user;
+      dispatch(loginUserSuccess(user)); //
+      Cookies.set("user", true);
+    } catch (err) {
+      console.error(err);
+      Cookies.set("user", false);
+    }
   };
 
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      dispatch(logoutuser());
+      Cookies.remove("user");
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <>
       <MyBox className="flex flex-col items-center justify-center p-4 rounded-2xl">
@@ -41,7 +90,7 @@ const LoginModel = ({ setLoginModel }) => {
             className="w-full border border-gray-300 rounded-md py-2 px-3 mb-6 focus:outline-none focus:border-blue-500"
           />
           <MyButton
-            myFunction={handleLogin}
+            myFunction={signIn}
             className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none"
           >
             Login
@@ -59,7 +108,7 @@ const LoginModel = ({ setLoginModel }) => {
             </span>
           </MyText>
           <MyButton
-            myFunction={handleLoginWithGoogle}
+            myFunction={signInWithGoogle}
             className="w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none mt-2"
           >
             Login with Google
