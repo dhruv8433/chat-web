ea31df288f41c7af8008af2e097fdb7103b9e60d
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-27 20:50:17 +0530

    created middleware

--- middleware.js
+++ middleware.js
@@ -1,9 +1,13 @@
-export default async function middleware(req) {
+import { NextResponse } from "next/server";
+
+export default async function middleware(req, res) {
   const { pathname } = req.nextUrl;
+  const url = "http://localhost:3000/";
 
   const isLoggedIn = req.cookies.get("user")?.value === "true";
 
-  if (!isLoggedIn) {
-    console.log("Welcoem bakc");
+  if (isLoggedIn && pathname.startsWith("/")) {
+    console.log("redirecting to " + url + "chats");
+    return NextResponse.redirect(url + "chats");
   }
 }
