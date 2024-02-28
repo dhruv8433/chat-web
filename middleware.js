e220367d06a00d1d2e54573c7e2a53bcf8401308
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-28 09:52:32 +0530

    middleware

--- middleware.js
+++ middleware.js
@@ -1,13 +1,18 @@
 import { NextResponse } from "next/server";
 
+let isRedirected = false;
+
 export default async function middleware(req, res) {
   const { pathname } = req.nextUrl;
   const url = "http://localhost:3000/";
 
   const isLoggedIn = req.cookies.get("user")?.value === "true";
 
-  if (isLoggedIn && pathname.startsWith("/")) {
-    console.log("redirecting to " + url + "chats");
-    return NextResponse.redirect(url + "chats");
+  // to run only once this middleware
+  if (!isRedirected && isLoggedIn && pathname.startsWith("/")) {
+    console.log("redirecting to " + url + "explore");
+    isRedirected = true;
+    return NextResponse.redirect(url + "explore");
   }
+
 }
