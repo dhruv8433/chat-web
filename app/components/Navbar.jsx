a7283b1e41cde12bfdffbd09e869a90d3872fcac
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-02-29 21:30:57 +0530

    navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,33 +1,29 @@
 "use client";
 
-import { Avatar, Box, useTheme } from "@mui/material";
-
 import React from "react";
 import Routes from "./Routes";
 import MyAvatar from "../common/MyAvatar";
 import { useSelector } from "react-redux";
+import MyBox from "../common/MyBox";
+import MyText from "../common/MyText";
 
 const Navbar = ({ toggleTheme }) => {
   const user = useSelector((state) => state.auth.authUser);
-  const theme = useTheme();
   return (
-    <Box
-      bgcolor={theme.palette.primary.main}
+    <MyBox
+      isPrimary={true}
       className="h-[90vh] p-2 rounded-2xl relative"
       sx={{ width: { xs: "100%", md: "100%" } }}
     >
       <div className="flex items-center p-4">
         {/* user image -- dynamic */}
-        <MyAvatar className="bg-cover" src={user.photoUrl}/>
-        <h1
-          className="ml-4 text-2xl"
-          style={{ fontWeight: 700, color: theme.palette.background.text }}
-        >
+        <MyAvatar className="bg-cover" src={user.photoUrl} />
+        <MyText className="ml-4 text-2xl" style={{ fontWeight: 700 }}>
           {user.displayName}
-        </h1>
+        </MyText>
       </div>
       <Routes toggleTheme={toggleTheme} />
-    </Box>
+    </MyBox>
   );
 };
 
