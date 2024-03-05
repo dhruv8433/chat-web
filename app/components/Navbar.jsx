503d6337f68a6a36c62caaa45d0ba4712fd47cb9
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-05 11:39:50 +0530

    navigation bar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,22 +8,24 @@ import MyBox from "../common/MyBox";
 import MyText from "../common/MyText";
 
 const Navbar = ({ toggleTheme }) => {
-  const user = useSelector((state) => state.auth.authUser);
+  const user = useSelector((state) => state.auth.authUser.data);
   return (
-    <MyBox
-      isPrimary={true}
-      className="h-[90vh] p-2 rounded-2xl relative"
-      sx={{ width: { xs: "100%", md: "100%" } }}
-    >
-      <div className="flex items-center p-4">
-        {/* user image -- dynamic */}
-        <MyAvatar className="bg-cover" src={user.photoUrl} />
-        <MyText className="ml-4 text-2xl" style={{ fontWeight: 700 }}>
-          {user.displayName}
-        </MyText>
-      </div>
-      <Routes toggleTheme={toggleTheme} />
-    </MyBox>
+    <div>
+      <MyBox
+        isPrimary={true}
+        className="h-[90vh] p-2 rounded-2xl relative"
+        sx={{ width: { xs: "100%", md: "100%" } }}
+      >
+        <div className="flex items-center p-4">
+          {/* user image -- dynamic */}
+          <MyAvatar className="bg-cover" src={user.photoUrl} />
+          <MyText className="ml-4 text-2xl font-bold" style={{ fontWeight: 700 }}>
+            {user.displayName}
+          </MyText>
+        </div>
+        <Routes toggleTheme={toggleTheme} />
+      </MyBox>
+    </div>
   );
 };
 
