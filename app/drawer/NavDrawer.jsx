51f507fb03fae75e24b0816b6559b82d930ef368
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-06 09:15:10 +0530

    navigation drawer

--- app/drawer/NavDrawer.jsx
+++ app/drawer/NavDrawer.jsx
@@ -7,11 +7,13 @@ const NavDrawer = () => {
   return (
     <div className="primary h-screen">
       <div className="flex p-2">
-       <MyAvatar/>
+        {/* user -- avatar */}
+        <MyAvatar />
         <div className="ml-4 text-white text-2xl">Hey, Partner</div>
       </div>
       <Divider />
       <div className="mt-4">
+        {/* all pages -- routes */}
         <Routes />
       </div>
     </div>
