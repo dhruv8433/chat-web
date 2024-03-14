63a9962c25c13eb42f8abb688b272690c704e3e9
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-14 09:47:12 +0530

    map

--- app/common/GeoapifyMap.jsx
+++ app/common/GeoapifyMap.jsx
@@ -3,24 +3,34 @@
 import React from "react";
 import maplibre from "maplibre-gl";
 import { useEffect } from "react";
+import { MAP_KEY } from "../config/config";
+import L from "leaflet";
 
-const GeoapifyMap = ({key}) => {
+const GeoapifyMap = ({ key }) => {
   let mapContainer;
   let map; // Store map instance
 
   useEffect(() => {
     const mapStyle = "https://maps.geoapify.com/v1/styles/osm-carto/style.json";
+    const lat = 20.5937;
+    const lng = 78.9629;
+
     const initialState = {
-      lat: 20.5937,
-      lng: 78.9629,
+      lat: lat,
+      lng: lng,
       zoom: 4,
     };
-const key=process.env.MAP_API_KEY;
+
     map = new maplibre.Map({
       container: mapContainer,
-      style: `${mapStyle}?apiKey=${key}`,
+      style: `${mapStyle}?apiKey=${MAP_KEY}`,
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom,
+    });
+
+    map.on("load", () => {
+      // Now that the map is loaded, you can add the marker
+      new maplibre.Marker().setLngLat([lat, lng]).addTo(map);
     });
 
     return () => {
