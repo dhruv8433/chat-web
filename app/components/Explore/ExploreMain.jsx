e99b09433b1a55b31ff80df28d7ca67a7f708ad0
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-14 09:47:24 +0530

    explore main page

--- app/components/Explore/ExploreMain.jsx
+++ app/components/Explore/ExploreMain.jsx
@@ -1,32 +1,33 @@
-import { Grid } from "@mui/material";
 import React from "react";
+import { Grid } from "@mui/material";
+import MyBox from "@/app/common/MyBox";
 import SwiperFirst from "./SwiperFirst";
+import NewsCardFirst from "./NewsCardFirst";
 import GeoapifyMap from "@/app/common/GeoapifyMap";
 
 const ExploreMain = () => {
-  const key = process.env.MAP_API_KEY;
-  console.log("here", key);
   return (
-    <div>
-      <Grid container spacing={1}>
+    <MyBox isPrimary={true} className="mt-4 rounded-2xl p-2">
+      {/* we provide categories here */}
+      <div className="">
+        articles
+        tourism
+        12-15
+      </div>
+
+      <Grid container spacing={3}>
         <Grid item xs={12} md={8}>
           <SwiperFirst />
+          <NewsCardFirst />
         </Grid>
 
         <Grid item xs={12} md={4}>
           <div className="mt-10 rounded-2xl">
-            <GeoapifyMap key={key} />
+            <GeoapifyMap />
           </div>
         </Grid>
-
-        {/* <Grid xs={12} md={6}>
-                    <NewsCardFirst />
-                </Grid>
-                <Grid xs={12} md={12}>
-                    <NewsCardSecond />
-                </Grid> */}
       </Grid>
-    </div>
+    </MyBox>
   );
 };
 
