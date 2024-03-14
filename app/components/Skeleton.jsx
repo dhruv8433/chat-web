f20890b678f717722e5ee77dfe68d9f32daf030b
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-14 09:47:16 +0530

    skeleton

---
+++ app/components/Skeleton.jsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "@mui/material";
+
+export const SwiperSkeleton = () => {
+  return (
+    <div>
+      <Skeleton variant="rectangular" width={"100%"} height={"500px"} />
+    </div>
+  );
+};
