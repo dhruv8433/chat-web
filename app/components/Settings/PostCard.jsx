d285e651b0850b9c3a2237d1d85be3d1a1c6792b
Varsani2520 <varsaniranjani2520@gmail.com>
2024-03-15 15:45:47 +0530

    post

--- app/components/Settings/PostCard.jsx
+++ app/components/Settings/PostCard.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+"use client"
+import { userAddPostServices } from "@/app/services/addUserPosts";
+import React, { useState } from "react";
 
 const PostCard = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("postImg", e.target.postImg.files[0]);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/add-post", {
-        method: "POST",
-        body: formData,
-      });
-      if (response.ok) {
+      const responseData = await userAddPostServices(formData); // Call service function with FormData
+      if (responseData && responseData.file && responseData.file.url) {
         console.log("File uploaded successfully!");
+        setImageUrl(responseData.file.url);
       } else {
         console.error("Failed to upload file");
       }
@@ -22,12 +24,14 @@ const PostCard = () => {
   };
   return (
     <div>
-      <form action ="http://localhost:8080/api/v1/add-post"onSubmit={handleSubmit} method="POST" encType="multipart/form-data">
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
 
         <input type="file" name="postImg" />
         <button type="submit">submit</button>
       </form>
+
       <h1>Post Info</h1>
+      {imageUrl && <img src={imageUrl} alt='Uploaded' />}
     </div>
   );
 };
--- app/explore/tourism-place/page.jsx
+++ app/explore/tourism-place/page.jsx
@@ -1,10 +1,33 @@
+import MyButton from '@/app/common/MyButton'
+import MyText from '@/app/common/MyText'
+import { Card, CardActionArea, CardMedia, TextField } from '@mui/material'
 import React from 'react'
 
 const page = () => {
+
   return (
     <div>
-      hlo
+
+
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <TextField
+          label="Enter a tourism place"
+          variant="outlined"
+          fullWidth
+
+        />
+        <MyButton>Search</MyButton>
+      </div>
+      <div>
+        <Card>
+          <CardMedia image='' />
+          <CardActionArea>
+            <MyText>city name</MyText>
+          </CardActionArea>
+        </Card>
+      </div>
     </div>
+    
   )
 }
 
