64e0624ec8539dc4e78d34bb22642a0966803ff1
Varsani2520 <varsaniranjani2520@gmail.com>
2024-03-15 17:29:56 +0530

    final add post

--- app/services/addUserPosts.js
+++ app/services/addUserPosts.js
@@ -1,10 +1,9 @@
+import axios from "axios";
 import { httpAxios } from "../httpAxios";
 
 export async function userAddPostServices(formData) {
   const result = await httpAxios
-    .post("/add-posts", {
-      formData: formData,
-    })
+    .post("/add-posts", formData) // Specify complete URL
     .then((response) => response.data);
   return result;
 }
