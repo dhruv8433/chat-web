9fcd32da2d585145c5a8a1d82deaeb359ac0d164
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-16 07:49:02 +0530

    tourism place

--- app/explore/tourism-place/page.jsx
+++ app/explore/tourism-place/page.jsx
@@ -1,34 +1,31 @@
-import MyButton from '@/app/common/MyButton'
-import MyText from '@/app/common/MyText'
-import { Card, CardActionArea, CardMedia, TextField } from '@mui/material'
-import React from 'react'
+import MyButton from "@/app/common/MyButton";
+import MyText from "@/app/common/MyText";
+import { Card, CardActionArea, CardMedia, TextField } from "@mui/material";
+import React from "react";
 
 const page = () => {
-
   return (
     <div>
-
-
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <TextField
-          label="Enter a tourism place"
-          variant="outlined"
-          fullWidth
-
-        />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TextField label="Enter a tourism place" variant="outlined" fullWidth />
         <MyButton>Search</MyButton>
       </div>
       <div>
         <Card>
-          <CardMedia image='' />
+          <CardMedia image="" />
           <CardActionArea>
             <MyText>city name</MyText>
           </CardActionArea>
         </Card>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default page
+export default page;
