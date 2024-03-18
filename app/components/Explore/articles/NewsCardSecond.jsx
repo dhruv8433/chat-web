b999a29258ee50efe633253e0a90763aeeac7361
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-18 08:10:38 +0530

    article cards

--- app/components/Explore/articles/NewsCardSecond.jsx
+++ app/components/Explore/articles/NewsCardSecond.jsx
@@ -1,67 +1,60 @@
-'use client'
-import Heading from '@/app/common/Heading';
-import NewsHeading from '@/app/common/NewsHeading';
-import { FAMILY_NEWS } from '@/app/config/config';
-import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography, useTheme } from '@mui/material';
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+"use client";
+
+import MyBox from "@/app/common/MyBox";
+import NewsHeading from "@/app/common/NewsHeading";
+import { getArticals } from "@/app/services/getArticles";
+import { Grid } from "@mui/material";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import slugify from "slugify";
+import NewsCardContent from "./NewsCardContent";
 
 const NewsCardSecond = () => {
-    const [home, setHome1] = useState([]);
+  const [articles, setArticles] = useState([]);
 
-    async function homecard1() {
-        try {
-            const response = await axios.get(FAMILY_NEWS);
-            console.log("my-card", response.data.articles.results);
-            setHome1(response.data.articles.results.slice(0, 4));
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
+  // getting top business articles
+  async function topBusiness() {
+    try {
+      const response = await getArticals("family");
+      setArticles(response.articles.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    const theme = useTheme()
-    useEffect(() => {
-        homecard1();
-    }, []);
+  }
 
-    const handleClick = (response) => {
-        localStorage.setItem("selectNews", JSON.stringify(response))
+  // call top business when page load
+  useEffect(() => {
+    topBusiness();
+  }, []);
 
-    }
-    return (
-        <div >
-            <NewsHeading title={"Family"} className="bg-red-400 " />
-            <Grid container spacing={2}>
-
-                {/* title */}
-                <div className=' flex mr-2 h-[500px] mb-9'>
-                    {home.map((news, index) => (
-                        <div className='w-[100%] mr-4'>
-                            <Grid item xs={12} sm={12} md={12} lg={12}>
-                                <Link href={`/explore/${encodeURIComponent(news.title)}`}>
-                                    <Card sx={{ maxWidth: 600 }} onClick={() => handleClick(news)} >
-                                        <CardActionArea>
-                                            <CardMedia
-                                                component="img"
-                                                height="140"
-                                                image={news.image}
-                                                alt={news.title}
-                                            />
-                                            <CardContent>
-                                                <Typography gutterBottom variant="h5" component="div">
-                                                    {news.title}
-                                                </Typography>
-                                            </CardContent>
-                                        </CardActionArea>
-                                    </Card>
-                                </Link>
-                            </Grid>
-                        </div>
-                    ))}
-                </div>
-            </Grid >
-        </div >
-    );
-}
+  // when user click any article we store it inside localstorage
+  const handleCardClick = (response) => {
+    localStorage.setItem("selectNews", JSON.stringify(response));
+  };
+  return (
+    <div>
+      <NewsHeading title={"Family"} className="bg-red-400 "/>
+      <Grid container spacing={2}>
+        {/* title */}
+        <div>
+          {articles.slice(0, 2).map((news, index) => (
+            <div className="w-[100%] mr-4" key={index}>
+              <Grid item xs={12}>
+                <Link href={`/explore/${slugify(news.title).toLowerCase()}`}>
+                  <MyBox
+                    className="flex justify-center p-5 my-4 h-[300px] rounded-2xl overflow-hidden"
+                    OnClick={handleCardClick(news)}
+                  >
+                    <NewsCardContent news={news} isSmallCard={true} />
+                  </MyBox>
+                </Link>
+              </Grid>
+            </div>
+          ))}
+        </div>
+      </Grid>
+    </div>
+  );
+};
 
 export default NewsCardSecond;
