d49b2335373accaa8a0ea218011e03b31d211593
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-26 07:25:07 +0530

    tourism place service

--- app/services/getLiveTouristSuggestion.js
+++ app/services/getLiveTouristSuggestion.js
@@ -1,16 +1,9 @@
-import axios from "axios";
-import { CUSTOM_RAPID_HEADERS, TOURISM_API } from "../config/config";
+import { httpAxios } from "../httpAxios";
 
 export async function getLiveTouristSuggestion(query) {
-  const response = await axios
-    .post(
-      `${TOURISM_API}/typeahead`,
-      { q: query, language: "en_US" },
-      {
-        headers: CUSTOM_RAPID_HEADERS,
-      }
-    )
-    .then((res) => res.data.results);
+  const response = await httpAxios
+    .get("/tourism/get-places")
+    .then((response) => response.data);
 
   return response;
 }
