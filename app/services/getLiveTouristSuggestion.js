187dd9ed1d2363c9bff77cee1c63bc8af94fa0d1
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-27 08:14:52 +0530

    tourism service

--- app/services/getLiveTouristSuggestion.js
+++ app/services/getLiveTouristSuggestion.js
@@ -1,8 +1,8 @@
 import { httpAxios } from "../httpAxios";
 
-export async function getLiveTouristSuggestion(query) {
+export async function getLiveTouristSuggestion(query, offset, limit) {
   const response = await httpAxios
-    .get("/tourism/get-places")
+    .get(`/tourism/get-places?_page=${offset}&_limit=${limit}`)
     .then((response) => response.data);
 
   return response;
