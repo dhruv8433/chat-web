9a9f14f27b4df0d2bf63cc323b6f513cc2ed0587
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-03-28 12:07:26 +0530

    explore categories

--- app/components/Explore/articles/ExploreMain.jsx
+++ app/components/Explore/articles/ExploreMain.jsx
@@ -1,11 +1,62 @@
 import React from "react";
 import MyBox from "@/app/common/MyBox";
+import MyText from "@/app/common/MyText";
+import Link from "next/link";
+import { Grid } from "@mui/material";
 
 const ExploreMain = () => {
   return (
-    <MyBox isPrimary={true} className="mt-4 rounded-2xl p-2">
-      {/* we provide categories here */}
-      <div className="">articles tourism 12-15...</div>
+    <MyBox isPrimary={true} className="mt-4 rounded-2xl p-4">
+      <Grid container>
+        <Grid item xs={12} md={4}>
+          {/* Tourism places */}
+          <Link href={"/explore/articles"}>
+            <div className="border rounded-2xl p-4 w-max">
+              <img
+                src="https://ds393qgzrxwzn.cloudfront.net/resize/m600x500/cat1/img/images/0/jwkvsCOubn.jpg"
+                className="object-cover h-[200px] w-[300px] rounded-2xl"
+                alt="bg"
+              />
+              <div className="mt-3">
+                <hr />
+                <MyText className={"text-xl font-bold"}>Articles</MyText>
+              </div>
+            </div>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          {/* Articles */}
+          <Link href={"/explore/health-and-fintess"}>
+            <div className="border rounded-2xl p-4 w-max">
+              <img
+                src="https://ds393qgzrxwzn.cloudfront.net/resize/m600x500/cat1/img/images/0/jwkvsCOubn.jpg"
+                className="object-cover h-[200px] w-[300px] rounded-2xl"
+                alt="bg"
+              />
+              <div className="mt-3">
+                <hr />
+                <MyText className={"text-xl font-bold"}>Health & Fitness</MyText>
+              </div>
+            </div>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          {/* Health & Fitness */}
+          <Link href={"/explore/tourism-place"}>
+            <div className="border rounded-2xl p-4 w-max">
+              <img
+                src="https://ds393qgzrxwzn.cloudfront.net/resize/m600x500/cat1/img/images/0/jwkvsCOubn.jpg"
+                className="object-cover h-[200px] w-[300px] rounded-2xl"
+                alt="bg"
+              />
+              <div className="mt-3">
+                <hr />
+                <MyText className={"text-xl font-bold"}>Tourism places</MyText>
+              </div>
+            </div>
+          </Link>
+        </Grid>
+      </Grid>
     </MyBox>
   );
 };
